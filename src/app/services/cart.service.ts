27d957e6a122d3611b8de8839c59c0957dcfc837
kentjuanfanshawe <k_juan@fanshaweonline.ca>
2022-11-23 12:07:54 -0500

    added ! in calculateCart

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -4,11 +4,15 @@ import { ShoppingCart } from '../components/models/cart';
 import { CartItem } from '../components/models/cart-item';
 import { Product } from '../components/models/product';
 import { ProductsService } from './products.service';
+import { LocalStorageService } from './storage.service';
+
+const CART_KEY: string = 'cart';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
+  private _storage: Storage;
   private _subscriptionObservable!: Observable<ShoppingCart>;
   private _subscribers: Array<Observer<ShoppingCart>> = new Array<
     Observer<ShoppingCart>
@@ -20,7 +24,11 @@ export class CartService {
   // added totalPrice
   totalPrice!: number;
 
-  constructor(private _productService: ProductsService) {
+  constructor(
+    private _productService: ProductsService,
+    private _storageService: LocalStorageService
+  ) {
+    this._storage = this._storageService.get();
     this._products = this._productService.getProducts();
     this._subscriptionObservable = new Observable<ShoppingCart>(
       (observer: Observer<ShoppingCart>) => {
@@ -54,30 +62,33 @@ export class CartService {
     // remove all the products that have a quantity equal to zero.
     cart.items = cart.items.filter((cartItem) => cartItem.quantity > 0);
 
-    // this.calculateCart(cart);
-    // this.save(cart)
+    this.calculateCart(cart);
+    this.save(cart);
     this.dispatch(cart);
   }
 
   private retrieve(): ShoppingCart {
     const cart = new ShoppingCart();
-
-    // TODO: replace with storage feature
-
+    const storedCart = this._storage.getItem(CART_KEY);
+    if (storedCart) {
+      cart.updateFrom(JSON.parse(storedCart));
+    }
     return cart;
   }
 
-  // private calculateCart(cart: ShoppingCart): void {
-  //   cart.itemsTotal = cart.items
-  //     .map(
-  //       (item) =>
-  //         item.quantity *
-  //         this._products.find((p) => p.id === item.productId).price
-  //     )
-  //     .reduce((previous, current) => previous + current, 0);
-  // }
+  private calculateCart(cart: ShoppingCart): void {
+    cart.itemsTotal = cart.items
+      .map(
+        (item) =>
+          item.quantity *
+          this._products.find((p) => p.id === item.productId)!.price
+      )
+      .reduce((previous, current) => previous + current, 0);
+  }
 
-  private save(cart: ShoppingCart): void {}
+  private save(cart: ShoppingCart): void {
+    this._storage.setItem(CART_KEY, JSON.stringify(cart));
+  }
 
   private dispatch(cart: ShoppingCart): void {
     this._subscribers.forEach((sub) => {
